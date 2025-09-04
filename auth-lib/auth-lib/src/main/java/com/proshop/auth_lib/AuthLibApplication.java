87f9b824dc219aa274515c52fc341fb12ddba883
tmd2311 <dung.kayc@gmail.com>
2025-09-04 17:20:25 +0700

    feat: thêm thư viện auth

---
+++ auth-lib/auth-lib/src/main/java/com/proshop/auth_lib/AuthLibApplication.java
@@ -0,0 +1,13 @@
+package com.proshop.auth_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthLibApplication.class, args);
+	}
+
+}
