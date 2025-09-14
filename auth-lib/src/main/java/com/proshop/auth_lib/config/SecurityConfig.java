14f6287184f60bad9c10785292195bc38c05235c
tmd2311 <dung.kayc@gmail.com>
2025-09-14 16:27:00 +0700

    fix: sửa logic cho phép các method GET không cần token

--- auth-lib/src/main/java/com/proshop/auth_lib/config/SecurityConfig.java
+++ auth-lib/src/main/java/com/proshop/auth_lib/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -66,6 +67,7 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable)
         .cors(cors -> cors.configurationSource(corsConfigurationSource))
         .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.GET, "/**").permitAll()
             .requestMatchers(PUBLIC_URLS).permitAll()
             .anyRequest().authenticated())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
