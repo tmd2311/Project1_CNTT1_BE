676b768325498e4b037e0d04a3d313aa089a573b
php0302 <85358615+php0302@users.noreply.github.com>
2025-10-05 00:00:49 +0700

    feat(auth-lib): Lấy userId từ token

--- auth-lib/src/main/java/com/proshop/auth_lib/utils/JwtUtil.java
+++ auth-lib/src/main/java/com/proshop/auth_lib/utils/JwtUtil.java
@@ -41,6 +41,8 @@ public class JwtUtil {
     return getClaims(token).get("user_code", String.class);
   }
 
+  public Long getUserIDFromToken(String token) { return getClaims(token).get("user_id", Long.class); }
+
   public boolean validateToken(String token) {
     try {
       getClaims(token);
