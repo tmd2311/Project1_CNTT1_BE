e4f0ed9e3ad4e5efca1a98d4cfc0a455fc22bb10
php0302 <85358615+php0302@users.noreply.github.com>
2025-10-13 15:31:16 +0700

    feat (product): Thêm,sửa ảnh vào product

--- product-service/product-service/src/main/java/com/proshop/product/dto/request/ProductCreateRequest.java
+++ product-service/product-service/src/main/java/com/proshop/product/dto/request/ProductCreateRequest.java
@@ -1,6 +1,8 @@
 package com.proshop.product.dto.request;
 
 import lombok.*;
+
+import java.util.List;
 import java.util.UUID;
 import java.util.Map;
 
@@ -15,4 +17,5 @@ public class ProductCreateRequest {
     private Map<String, Object> specs; // nếu bạn dùng JSON
     private UUID brandId;
     private UUID categoryId;
+    private List<String> imageUrls; // <== thêm mới
 }
--- product-service/product-service/src/main/java/com/proshop/product/repository/ProductImageRepository.java
+++ product-service/product-service/src/main/java/com/proshop/product/repository/ProductImageRepository.java
@@ -2,8 +2,16 @@ package com.proshop.product.repository;
 
 import com.proshop.product.entity.ProductImageEntity;
 import java.util.UUID;
+
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductImageRepository extends JpaRepository<ProductImageEntity, UUID> {
-
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ProductImageEntity p WHERE p.product.id = :productId")
+    void deleteAllByProductId(@Param("productId") UUID productId);
 }
