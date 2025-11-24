4dc42dc6f562b1bba4dd0f7765037be1ba222e13
tmd2311 <dung.kayc@gmail.com>
2025-11-25 00:32:52 +0700

    fix (deploy) : fix lỗi thư viện

--- sale-service/src/main/java/com/proshop/sale_service/SaleServiceApplication.java
+++ sale-service/src/main/java/com/proshop/sale_service/SaleServiceApplication.java
@@ -2,10 +2,15 @@ package com.proshop.sale_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"com.proshop.sale_service",
+	"com.proshop.auth_lib"
+})
 @EnableScheduling
+@EnableFeignClients(basePackages = "com.proshop.sale_service.client")
 public class SaleServiceApplication {
 
 	public static void main(String[] args) {
