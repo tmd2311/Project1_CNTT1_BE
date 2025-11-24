b3bd1ed67079a81276aa3a545de2add631155d36
tmd2311 <dung.kayc@gmail.com>
2025-11-25 00:45:40 +0700

    fix (deploy) : xóa config thừa

--- sale-service/src/main/java/com/proshop/sale_service/config/FeignConfig.java
+++ sale-service/src/main/java/com/proshop/sale_service/config/FeignConfig.java
@@ -1,7 +1,6 @@
 package com.proshop.sale_service.config;
 
 import feign.Logger;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
  * Configuration cho Feign Client
  */
 @Configuration
-@EnableFeignClients(basePackages = "com.proshop.sale_service.client")
 public class FeignConfig {
 
     @Bean
